bfcdbadef7c422a4caa81c0eb0382d92305cda8f
keencho <seyoung050412@gmail.com>
2023-05-28 19:03:36 +0900

    init electron playground

---
+++ src/renderer/components/Main/Main.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const St = styled.div`
+  color: red;
+`
+
+const Main = () => {
+  return (
+    <St>Hello!</St>
+  )
+}
+
+export default Main
---
+++ src/renderer/global.style.ts
@@ -0,0 +1,11 @@
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    color: red !important;
+    margin: 0;
+    padding: 0;
+  }
+`
+
+export default GlobalStyle
